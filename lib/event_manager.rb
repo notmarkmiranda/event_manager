fbd6e379ffce1a9a085955673a246c3250d34018
Mark Miranda <notmarkmiranda@gmail.com>
2016-02-28 13:27:39 -0700

    working on time/date parsing

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'sunlight/congress'
 require 'erb'
+require 'pry'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -9,12 +10,22 @@ def clean_zipcode(zipcode)
 end
 
 def clean_phone_number(phonenumber)
-  phonenumber.chars.select { |digit| digit if digit =~ /[0-9]/ }.join
-  validate_phone_number(phonenumber)
+  validate_phone_number(phonenumber.chars.select { |digit| digit if digit =~ /[0-9]/ }.join)
 end
 
 def validate_phone_number(phonenumber)
-  phonenumber.slice!(0) if phonenumber.length == 11 && phonenumber[0] == "1"
+  if (phonenumber.length == 11 && phonenumber[0] == "1")
+    phonenumber = phonenumber[-10..-1]
+  elsif phonenumber.length == 10 && phonenumber[0] != "1"
+    phonenumber
+  else
+    phonenumber = "0000000000"
+  end
+  format_phone_number(phonenumber)
+end
+
+def format_phone_number(phonenumber)
+  phonenumber = phonenumber.insert(3, "-").insert(7, "-")
 end
 
 def legislators_by_zipcode(zipcode)
@@ -43,10 +54,10 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phonenumber = clean_phone_number(row[:homephone])
+  regdate = Date.strptime(row[:regdate], "%m/%d/%y %H:%M")
+  # THIS IS WHERE I STOPPED
+
   legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letters(id, form_letter)
-
+  # form_letter = erb_template.result(binding)
+  # save_thank_you_letters(id, form_letter)
 end
